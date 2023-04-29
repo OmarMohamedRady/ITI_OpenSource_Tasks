e905ed03285820507b8f8cc13c6bb0c86d33dab1
Omar Mohamed <omarmohamedradi97@gmail.com>
2023-04-29 17:47:49 +0300

    Bash Lab task2

--- Bash/Lab1/task2.sh
+++ Bash/Lab1/task2.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+#execute the script  every 1 min to monitor system load, and add it to file /var/log/system-load.PS: The script must be run using root.
+##Exit codes:
+##	0 : Normal terminated
+##	1: The user is not the root
 
 # check if the user who run the script is the root
 [ ${EUID} -ne 0 ] && exit 1
